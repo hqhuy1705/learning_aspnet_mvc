f04e57795aa25b13b5fede24b80c7ad0bf0937f2
Huy <hqhuy1705@gmail.com>
2022-04-02 23:11:37 +0700

    update script database

--- Database/Initial_Database.sql
+++ Database/Initial_Database.sql
@@ -2,79 +2,79 @@ USE [master]
 GO
 /****** Object:  Database [learning_apsnet_mvc]    Script Date: 2/4/2022 10:57:30 PM ******/
 CREATE DATABASE [learning_apsnet_mvc]
- CONTAINMENT = NONE
- ON  PRIMARY 
-( NAME = N'learning_apsnet_mvc', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL12.SQLEXPRESS\MSSQL\DATA\learning_apsnet_mvc.mdf' , SIZE = 4096KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
- LOG ON 
-( NAME = N'learning_apsnet_mvc_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL12.SQLEXPRESS\MSSQL\DATA\learning_apsnet_mvc_log.ldf' , SIZE = 2048KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET COMPATIBILITY_LEVEL = 130
-GO
-IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
-begin
-EXEC [learning_apsnet_mvc].[dbo].[sp_fulltext_database] @action = 'enable'
-end
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET ANSI_NULL_DEFAULT OFF 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET ANSI_NULLS OFF 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET ANSI_PADDING OFF 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET ANSI_WARNINGS OFF 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET ARITHABORT OFF 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET AUTO_CLOSE OFF 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET AUTO_SHRINK OFF 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET AUTO_UPDATE_STATISTICS ON 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET CURSOR_CLOSE_ON_COMMIT OFF 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET CURSOR_DEFAULT  GLOBAL 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET CONCAT_NULL_YIELDS_NULL OFF 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET NUMERIC_ROUNDABORT OFF 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET QUOTED_IDENTIFIER OFF 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET RECURSIVE_TRIGGERS OFF 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET  DISABLE_BROKER 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET DATE_CORRELATION_OPTIMIZATION OFF 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET TRUSTWORTHY OFF 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET ALLOW_SNAPSHOT_ISOLATION OFF 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET PARAMETERIZATION SIMPLE 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET READ_COMMITTED_SNAPSHOT OFF 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET HONOR_BROKER_PRIORITY OFF 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET RECOVERY SIMPLE 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET  MULTI_USER 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET PAGE_VERIFY CHECKSUM  
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET DB_CHAINING OFF 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET TARGET_RECOVERY_TIME = 0 SECONDS 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET DELAYED_DURABILITY = DISABLED 
-GO
-ALTER DATABASE [learning_apsnet_mvc] SET QUERY_STORE = OFF
-GO
+-- CONTAINMENT = NONE
+-- ON  PRIMARY 
+--( NAME = N'learning_apsnet_mvc', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL12.SQLEXPRESS\MSSQL\DATA\learning_apsnet_mvc.mdf' , SIZE = 4096KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
+-- LOG ON 
+--( NAME = N'learning_apsnet_mvc_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL12.SQLEXPRESS\MSSQL\DATA\learning_apsnet_mvc_log.ldf' , SIZE = 2048KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET COMPATIBILITY_LEVEL = 130
+--GO
+--IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+--begin
+--EXEC [learning_apsnet_mvc].[dbo].[sp_fulltext_database] @action = 'enable'
+--end
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET ANSI_NULL_DEFAULT OFF 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET ANSI_NULLS OFF 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET ANSI_PADDING OFF 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET ANSI_WARNINGS OFF 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET ARITHABORT OFF 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET AUTO_CLOSE OFF 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET AUTO_SHRINK OFF 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET AUTO_UPDATE_STATISTICS ON 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET CURSOR_CLOSE_ON_COMMIT OFF 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET CURSOR_DEFAULT  GLOBAL 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET CONCAT_NULL_YIELDS_NULL OFF 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET NUMERIC_ROUNDABORT OFF 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET QUOTED_IDENTIFIER OFF 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET RECURSIVE_TRIGGERS OFF 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET  DISABLE_BROKER 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET DATE_CORRELATION_OPTIMIZATION OFF 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET TRUSTWORTHY OFF 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET PARAMETERIZATION SIMPLE 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET READ_COMMITTED_SNAPSHOT OFF 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET HONOR_BROKER_PRIORITY OFF 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET RECOVERY SIMPLE 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET  MULTI_USER 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET PAGE_VERIFY CHECKSUM  
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET DB_CHAINING OFF 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET TARGET_RECOVERY_TIME = 0 SECONDS 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET DELAYED_DURABILITY = DISABLED 
+--GO
+--ALTER DATABASE [learning_apsnet_mvc] SET QUERY_STORE = OFF
+--GO
 USE [learning_apsnet_mvc]
 GO
 ALTER DATABASE SCOPED CONFIGURATION SET LEGACY_CARDINALITY_ESTIMATION = OFF;
